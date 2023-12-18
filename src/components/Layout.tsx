9c9dda001e9a9d86a6c3815a6e8dbf2b33b47e0e
hani <ploo188000@gmail.com>
2023-12-18 10:34:15 +0900

    [Layout] search css

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import Nav from "./nav/Nav";
 import { LayoutProps } from "@/types/propsTypes";
-import style from "./layout.module.css";
 
 const Layout = ({ children }: LayoutProps) => {
     return (
-        <div className="layout bg-gradient-to-b from-[#201820] via-[#413e52] to-[#626093] max-w-[600px] min-w-[375px] mx-auto border h-screen">
-            <Nav />
+        <div className="max-w-[600px] min-w-[375px] mx-auto h-screen">
             <div>{children}</div>
         </div>
     );
