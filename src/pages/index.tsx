615f81e6ed8a23d5aa83535460e8aebc249a80eb
hani <ploo188000@gmail.com>
2024-01-04 16:19:38 +0900

    [Features] post detail

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,5 @@
-import React, { useEffect } from "react";
-import { useRouter } from "next/router";
-import { useAuth } from "../context/KakaoContext";
+import AboutUs from "./aboutus";
 
 export default function Home() {
-    const router = useRouter();
-    const auth = useAuth();
-
-    if (!auth) {
-        return null;
-    }
-
-    const { userInfo, isLoggedIn, logout } = auth;
-
-    const onLogin = () => {
-        router.replace("/users/login");
-    };
-
-    console.log("userInfo", userInfo);
-    console.log("isLoggedIn", isLoggedIn);
-    console.log("logout", logout);
-
-    return (
-        <div>
-            <div className='foolish'>남규 바보</div>
-            {isLoggedIn ? (
-                <button type='button' onClick={logout}>
-                    로그아웃
-                </button>
-            ) : (
-                <button type='button' onClick={onLogin}>
-                    로그인
-                </button>
-            )}
-        </div>
-    );
+    return (<AboutUs />);
 }
