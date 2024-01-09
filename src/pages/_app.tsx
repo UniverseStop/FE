09e51b7d2624d179b3397f3b3a3b90501370aae4
tph7897 <132332533+tph7897@users.noreply.github.com>
2024-01-09 09:46:57 +0900

    [Features] main-filter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,14 @@ import { useLoading } from "@/hooks/useLoading";
 import Spinner from "@/components/spinner/Spinner";
 
 export default function App({ Component, pageProps }: AppProps) {
-    const queryClient = new QueryClient();
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: false, // 윈도우가 다시 포커스되었을때 데이터를 호출할 것인지
+                retry: 0, // API 요청 실패시 재시도 하는 옵션 (설정값 만큼 재시도)
+            },
+        },
+    });
     // const { session } = pageProps;
     const isLoading = useLoading();
 
@@ -19,7 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <AuthProvider>
                 <QueryClientProvider client={queryClient}>
                     {/* <SessionProvider session={session}> */}
-                    <ReactQueryDevtools initialIsOpen={false} />
+                    <ReactQueryDevtools />
                     <Layout>
                         {isLoading ? <Spinner /> : null}
                         <Component {...pageProps} />
