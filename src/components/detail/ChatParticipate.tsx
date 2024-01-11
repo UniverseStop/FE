d4f8108667556f2188457681557b23bc086cf260
hani <ploo188000@gmail.com>
2024-01-11 17:12:05 +0900

    [Features] 정류장 상세 참가 수락 및 거절 기능 완성

--- src/components/detail/ChatParticipate.tsx
+++ src/components/detail/ChatParticipate.tsx
@@ -1,20 +1,38 @@
 import { postChatApproval, postChatRefuse } from "@/pages/api/post";
 import { ChatApprovalType } from "@/types/postTypes";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import UserInfo from "./UserInfo";
 
 const ChatParticipate = ({applicants, postId, userId}: {applicants: ChatApprovalType[], postId: number, userId: number}) => {
+    console.log(applicants)
     // 아래 모두 본인 작성글에서만 가능
     // 참여 승인
-    const approvalMutation = useMutation(postChatApproval, {});
-    const handleClickApproval = () => {
-        approvalMutation.mutate({postId, userId});
+    const queryClient = useQueryClient();
+    const approvalMutation = useMutation(postChatApproval, {
+        onSuccess: () => {
+            queryClient.invalidateQueries("post");
+            alert("참가 수락을 성공했습니다.");
+        },
+        onError: () => {
+            alert("참가 수락을 실패했습니다.");
+        },
+    });
+    const handleClickApproval = (id: number) => {
+        approvalMutation.mutate({postId, userId: id});
     };
 
     // 참여 거절 
-    const chatRefuseMutation = useMutation(postChatRefuse, {});
-    const handleClickRefuse = () => {
-        chatRefuseMutation.mutate({postId, userId});
+    const chatRefuseMutation = useMutation(postChatRefuse, {
+        onSuccess: () => {
+            queryClient.invalidateQueries("post");
+            alert("참가 거절을 성공했습니다.");
+        },
+        onError: () => {
+            alert("참가 거절을 실패했습니다.");
+        },
+    });
+    const handleClickRefuse = (id: number) => {
+        chatRefuseMutation.mutate({postId, userId: id});
     };
 
     return (
@@ -24,9 +42,9 @@ const ChatParticipate = ({applicants, postId, userId}: {applicants: ChatApproval
                 return (
                     <div key={info.userId} className="flex justify-between items-center">
                         <UserInfo userId={info.userId} nickname={info.nickname} age={info.age} gender={info.gender} imageUrl={info.profileImageUrl}/>
-                        <div className="text-sm font-bold text-white mr-7">
-                            <button onClick={()=>handleClickApproval()} className="w-16 h-9 bg-mainColor rounded-2xl">참가수락</button>
-                            <button onClick={()=>handleClickRefuse()} className="w-16 h-9 bg-mainColor rounded-2xl">참가거절</button>
+                        <div className="text-sm font-bold text-white mr-7 space-x-2">
+                            <button onClick={()=>handleClickApproval(info.userId)} className="w-16 h-9 bg-mainColor rounded-2xl">참가수락</button>
+                            <button onClick={()=>handleClickRefuse(info.userId)} className="w-16 h-9 bg-mainColor rounded-2xl">참가거절</button>
                         </div>
                     </div>
                 );
