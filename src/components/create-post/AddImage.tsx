2ebfc2a1c0048a80f1107c98c9611c774be6ae72
hotcream3904 <130561236+hotcream3904@users.noreply.github.com>
2024-01-18 15:51:20 +0900

    [Refactor] 이미지삭제버튼 추가

--- src/components/create-post/AddImage.tsx
+++ src/components/create-post/AddImage.tsx
@@ -1,9 +1,28 @@
 import Image from "next/image";
 import React, { ChangeEvent, useState } from "react";
+import { FiTrash } from "react-icons/fi";
 
 function AddImage({ postImage, setPostImage }: { postImage: File[]; setPostImage: (postImage: File[]) => void }) {
 	const [imagePreview, setImagePreview] = useState<File[]>([]);
 	const [isButtonDisabled, setIsButtonDisabled] = useState<Boolean>(true);
+	const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  	const handleMouseEnter = (index: number) => {
+  	  setHoveredIndex(index);
+  	};
+ 	 const handleMouseLeave = () => {
+  	  setHoveredIndex(null);
+ 	 };
+
+	  const handleDeleteImage = () => {
+		if (hoveredIndex !== null) {
+		  const updatedImagePreview = [...imagePreview];
+		  updatedImagePreview.splice(hoveredIndex, 1);
+		  setImagePreview(updatedImagePreview);
+		  setIsButtonDisabled(!(updatedImagePreview.length >= 3));
+		}
+	  };
+
 
 	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		if (imagePreview.length >= 2) {
@@ -31,32 +50,25 @@ function AddImage({ postImage, setPostImage }: { postImage: File[]; setPostImage
 		<div>
 			<p className="text-2xl font-bold m-6 0 6 6">☝🏻표지에 쓰일 이미지를 골라주세요!</p>
 			<div className="flex gap-3 ml-6">
-				{imagePreview &&
-					imagePreview.map((file, i) => (
-						<div key={i}>
-							<img
-								className="rounded-2xl object-cover h-36 w-36"
-								src={URL.createObjectURL(file)}
-								alt={`image${i}`}
-							/>
-						</div>
-					))}
-				<label
-					htmlFor="file"
-					className={`flex justify-center items-center border border-mainColor rounded-2xl w-36 h-36 cursor-pointer ${
-						!isButtonDisabled ? "pointer-events-none opacity-50" : ""
-					}`}>
-					<Image alt="사진업로드아이콘" width={45} height={45} src="/images/AddImageIcon.png" />
-				</label>
-				<input
-					type="file"
-					id="file"
-					multiple
-					accept=".jpg, .jpeg, .png"
-					placeholder="제목을 작성해주세요"
-					className="hidden"
-					onChange={handleChange}
-				/>
+					{imagePreview && imagePreview.map((file, i) => (
+          			  <div key={i} onMouseEnter={() => handleMouseEnter(i)} onMouseLeave={handleMouseLeave} >
+            			  <img className="rounded-2xl object-cover h-36 w-36" src={URL.createObjectURL(file)} alt={`image${i}`}/>
+           		     {hoveredIndex === i && (
+						<div className="absolute top-[36%] flex justify-center items-center rounded-2xl h-36 w-36 bg-black bg-opacity-50">
+                	    	<button onClick={handleDeleteImage}>
+                 			   <FiTrash className="w-5 h-5 text-white"/>
+                  		    </button>
+                       </div>
+             	     )}
+            		  </div>
+          			))}
+					<label
+						htmlFor="file"
+						className={`flex justify-center items-center border border-mainColor rounded-2xl w-36 h-36 cursor-pointer
+						${ !isButtonDisabled ? "hidden" : ""}`}>
+						<Image alt="사진업로드아이콘" width={45} height={45} src="/images/AddImageIcon.png" />
+				    </label>
+					<input type="file" id="file" multiple accept=".jpg, .jpeg, .png" placeholder="제목을 작성해주세요" className="hidden" onChange={handleChange}/>
 			</div>
 		</div>
 	);
