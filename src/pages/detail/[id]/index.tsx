c26c299136aaca54c9214771f6df4fb0006f5488
hotcream3904 <130561236+hotcream3904@users.noreply.github.com>
2024-02-14 18:11:40 +0900

    [Feature]상세페이지  SSR 구현

--- src/pages/detail/[id]/index.tsx
+++ src/pages/detail/[id]/index.tsx
@@ -1,13 +1,49 @@
 import { useRouter } from "next/router";
 import Post from "@/components/detail/Post";
+import { getBusStopDetail } from "@/pages/api/post";
+import { dehydrate, QueryClient } from "react-query";
 
-export default function Detail() {
-    const router = useRouter();
-    const { query } = router; 
+export default function Detail(props: any) {
+    const data = props.dehydratedState.queries[0].state.data
 
     return (
         <div>
-            {query && query.id && <Post postId={Number(query.id)}/>}
+            {data && <Post props={data}/>}
         </div>
     )
 }
+
+export const getServerSideProps = async (context : any) => {
+    try {
+    const { id } = context.params;
+    const queryClient = new QueryClient();
+    await queryClient.fetchQuery(["post", id], () => getBusStopDetail(id));
+
+    return {
+        props: {
+            dehydratedState: dehydrate(queryClient),
+        },
+    };
+} catch(error) {
+    console.log(error)
+}
+};
+
+// export const getServerSideProps: GetServerSideProps = async (context): Promise<GetServerSidePropsResult<{ [key: string]: any; }>> => {
+//     console.log("context", context)
+//     try {
+
+//         const queryClient = new QueryClient();
+//         await queryClient.prefetchQuery("post",()=>getBusStopDetail(id));
+//         return {
+//             props: {
+//                 dehydratedProps: dehydrate(queryClient)
+//             },
+//         };
+//     } catch (error) {
+//         console.log(error);
+//         return {
+//             props: {},
+//         };
+//     }
+// };
